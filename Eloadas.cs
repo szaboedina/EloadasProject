e27e84692a6a5aa8839e93aa1dd0ba5b1aeb40f9
Edina Szabó <szabo.edina.1229@gmail.com>
2020-03-04 12:29:13 +0100

    tesztesetek

--- Eloadas.cs
+++ Eloadas.cs
@@ -1,9 +1,72 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace EloadasProject
 {
     public class Eloadas
     {
+        bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if (sorokSzama < 1 || helyekSzama < 1)
+            {
+                throw new ArgumentException("A számoknak pozitívnak kell, hogy lenniük.");
+            }
+            
+            this.foglalasok = new bool[sorokSzama,helyekSzama];
+
+            
+
+        }
+        public bool Lefoglal() {
+            
+                for (int i = 0; i < foglalasok.GetLength(0); i++)
+                {
+                    for (int j = 0; j < foglalasok.GetLength(1); j++)
+                    {
+                        if (foglalasok[i, j] == false) {
+                            foglalasok[i, j] = true;
+                            return true;
+                        }
+                    }
+                }
+            return false;   
+        }
+
+
+        public int SzabadHelyek() {
+            int szabadHelyekSzama = 0;
+                for (int i = 0; i < foglalasok.GetLength(0); i++)
+                {
+                    for (int j = 0; j < foglalasok.GetLength(1); j++)
+                    {
+                        if (foglalasok[i, j] == false)
+                        {
+                            szabadHelyekSzama++;
+                        }
+                    }
+                }
+            return szabadHelyekSzama;
+
+        }
+        public bool Teli() {
+            if (SzabadHelyek() == 0) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam) {
+            if (foglalasok[sorSzam, helySzam] == true) {
+                return false;
+            }
+            return true;
+        }
+
 
     }
 }
---
+++ Tests/EloadasokTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EloadasProject.Tests
+{
+    [TestFixture]
+    class EloadasokTest
+    {
+        Eloadas e;
+        [SetUp]
+        public void SetUp()
+        {
+            e = new Eloadas(3, 7);
+        }
+
+        [TestCase]
+        //új előadás pozitív értékekkel
+        public void UjEloadas() {
+            Eloadas elsoEloadas = new Eloadas(3, 6);
+        }
+        [TestCase]
+        //új előadás, az egyik érték 0
+        public void UjEloadas2() {
+            Assert.Throws<ArgumentException>(() => { Eloadas masodikEloadas = new Eloadas(0, 6); });
+        }
+
+        [TestCase]
+        //új előadás negatív értékekkel
+        public void UjEloadas3()
+        {
+            Assert.Throws<ArgumentException>(() => { Eloadas harmadikEloadas = new Eloadas(-14, -12432); });
+        }
+        [TestCase]
+        public void Foglalas()
+        {
+            Assert.IsTrue(e.Lefoglal());
+        }
+
+        [TestCase]
+        public void FoglaltHely() {
+            Assert.IsTrue(e.Foglalt(1, 1));
+        }
+    }
+}
